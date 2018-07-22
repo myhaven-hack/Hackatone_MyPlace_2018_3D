a567352a6747696f4123cf1cdfaf05bd55ace0a4
myhaven-hack <41349589+myhaven-hack@users.noreply.github.com>
2018-07-21 22:43:19 -0400

    Added Operation Token and Device IDs

--- Authorization_File.py
+++ Authorization_File.py
@@ -1,8 +1,15 @@
+
 api = {
   "resultCode": 0,
   "key": "MQ-5r7R3-ewEsM8x7Tc1ZXYFIjSkiaZiOAUVTlHANegLw-EaSAk3XTAlmtc-AAkJ",
   "keyExpire": "2030-01-01T05:00:00Z",
   "keyExpireMs": 1893474000000
+}
+
+op_token = {
+	"ppc": ("dOfwlR1aCa4IZ3bZK1Gs_zEPR82xrGnplXBlnR_r0CFuxCXKjkTkBxp2UGpuNRFN"
+			"TYj33lXORvO7EyOlyjNI9rAcDRy7_5_xgmbsjS_-wVjXvcHqdEVRtPNbWuiZjF"
+			"V0A1J2Ok4r9cGn9K4Cq4JJPknsOTyTe5_e6WyjN_kjPuI")
 }
 
 device_ids = {
